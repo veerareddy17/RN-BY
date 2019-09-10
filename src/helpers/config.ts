e3d780edba8a49b32620b6d002c78f70c8a7d1b1
Appu <appu.patil@goavega.com>
2019-09-10 12:11:22 +0000

    Squashed commit of the following
    commit a25b43e8 
    Author: Appu Patil <apatil@goavega.com> 
    Date: Tue Sep 10 2019 17:39:30 GMT+0530 (India Standard Time) 
    
        temp release key store file
    
    
    commit d688374c 
    Author: Appu Patil <apatil@goavega.com> 
    Date: Tue Sep 10 2019 17:28:34 GMT+0530 (India Standard Time) 
    
        build issue fixes
    
    
    commit ea713263 
    Author: Appu Patil <apatil@goavega.com> 
    Date: Mon Sep 09 2019 21:11:15 GMT+0530 (India Standard Time) 
    
        Added loader on dashboard
    
    
    commit 1d126237 
    Author: Appu Patil <apatil@goavega.com> 
    Date: Mon Sep 09 2019 20:46:00 GMT+0530 (India Standard Time) 
    
        Removed console logs
    
    
    commit a1b41ffb 
    Author: Appu Patil <apatil@goavega.com> 
    Date: Mon Sep 09 2019 19:59:45 GMT+0530 (India Standard Time) 
    
        Resolved merge conflicts
    
    
    commit 6421c2b0 
    Author: Appu Patil <apatil@goavega.com> 
    Date: Mon Sep 09 2019 19:45:04 GMT+0530 (India Standard Time) 
    
        Dashboard filter API integration
    
    
    commit 9a5700a3 
    Author: Appu Patil <apatil@goavega.com> 
    Date: Sat Sep 07 2019 18:14:46 GMT+0530 (India Standard Time) 
    
        Dashboard Lead report api integrated

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,7 +1,7 @@
 export default {
     api: {
-        //baseURL: 'http://byjus-crm-qa.us-east-2.elasticbeanstalk.com/api',
-        baseURL: 'https://77b297eb.ngrok.io/api',
+        baseURL: 'http://byjus-crm-qa.us-east-2.elasticbeanstalk.com/api',
+        // baseURL: 'https://afd5c708.ngrok.io/api',
     },
     otp: {
         length: 4,
--- src/redux/actions/otp-actions.ts
+++ src/redux/actions/otp-actions.ts
@@ -3,10 +3,10 @@ import { OTP_SENT, LOAD_OTP_START, LOAD_OTP_FAIL, LOAD_OTP_INIT } from './action
 import { OTPRequest } from './../../models/request/otp-request';
 import { LeadService } from './../../services/lead-service';
 import { Dispatch } from 'redux';
-import generateOTP from "../../helpers/otp-creation";
+import generateOTP from '../../helpers/otp-creation';
 import StorageService from '../../database/storage-service';
 import { StorageConstants } from '../../helpers/storage-constants';
-import SendSMS from 'react-native-sms'
+import SendSMS from 'react-native-sms';
 import config from '../../helpers/config';
 
 export const otpStartAction = () => {
@@ -21,14 +21,14 @@ export const otpInitAction = () => {
     };
 };
 
-export const otpFailureAction = (error) => {
+export const otpFailureAction = error => {
     return {
         type: LOAD_OTP_FAIL,
         payload: error,
     };
 };
 
-export const otpSuccessAction = (successData) => {
+export const otpSuccessAction = successData => {
     return {
         type: OTP_SENT,
         payload: successData,
@@ -36,7 +36,6 @@ export const otpSuccessAction = (successData) => {
 };
 
 export const verifyOTP = (phone: string, connection: boolean) => async (dispatch: Dispatch) => {
-
     dispatch(otpStartAction());
     let OTP = await generateOTP();
     console.log('OTP generated - ', OTP);
@@ -56,82 +55,51 @@ export const verifyOTP = (phone: string, connection: boolean) => async (dispatch
                 dispatch(otpFailureAction(response.errors));
             }
         } else {
-            console.log('else');
-            console.log('request in sendSMS', otpRequest)
             return new Promise((resolve, reject) => {
-                SendSMS.send({
-                    //Message body
-                    body: config.offlineMessageBody + otpRequest.code,
-                    //Recipients Number
-                    recipients: [otpRequest.phone],
-                    //An array of types that would trigger a "completed" response when using android
-                    successTypes: ['sent', 'queued']
-                }, (completed, cancelled, error) => {
-                    if (completed) {
-                        console.log('SMS Sent Completed');
-                        dispatch(otpSuccessAction({ success: true }));
-                        resolve(true);
-                    } else if (cancelled) {
-                        console.log('SMS Cancelled');
-                        dispatch(otpFailureAction('SMS Sent Cancelled'));
-                        reject(reject);
-                    } else if (error) {
-                        console.log('SMS error');
-                        dispatch(otpFailureAction('Some error occured'));
-                        reject(reject);
-                    }
-                });
-
-                //await sendSMS(otpRequest);
-
-                // let response = await SMSService.sendSMS(otpRequest);
-                // console.log('response sms service', response);
+                SendSMS.send(
+                    {
+                        //Message body
+                        body: config.offlineMessageBody + otpRequest.code,
+                        //Recipients Number
+                        recipients: [otpRequest.phone],
+                        //An array of types that would trigger a "completed" response when using android
+                        successTypes: ['sent', 'queued'],
+                    },
+                    (completed, cancelled, error) => {
+                        if (completed) {
+                            console.log('SMS Sent Completed');
+                            dispatch(otpSuccessAction({ success: true }));
+                            resolve(true);
+                        } else if (cancelled) {
+                            console.log('SMS Cancelled');
+                            dispatch(otpFailureAction('SMS Sent Cancelled'));
+                            reject(reject);
+                        } else if (error) {
+                            console.log('SMS error');
+                            dispatch(otpFailureAction('Some error occured'));
+                            reject(reject);
+                        }
+                    },
+                );
             });
         }
-
     } catch (error) {
         // Error
         console.log(error);
     }
 };
 
-// export const sendSMS = (otpRequest: OTPRequest) => async (dispatch: Dispatch) => {
-//     console.log('request in sendSMS', otpRequest)
-//     await SendSMS.send({
-//         //Message body
-//         body: config.offlineMessageBody + otpRequest.code,
-//         //Recipients Number
-//         recipients: [otpRequest.phone],
-//         //An array of types that would trigger a "completed" response when using android
-//         successTypes: ['sent', 'queued']
-//     }, (completed, cancelled, error) => {
-//         if (completed) {
-//             //console.log('SMS Sent Completed');
-//             dispatch(otpSuccessAction({ success: true }));
-//         } else if (cancelled) {
-//             dispatch(otpFailureAction('SMS Sent Cancelled'));
-//         } else if (error) {
-//             dispatch(otpFailureAction('Some error occured'));
-//         }
-//     });
-// }
-
-
 export const submitOTP = (otp: String) => async (dispatch: Dispatch) => {
     dispatch(otpStartAction());
     let storedOTP = await StorageService.get<string>(StorageConstants.USER_OTP);
-    console.log('stored otp', storedOTP);
-    console.log('entered', otp);
     if (otp === storedOTP) {
         dispatch(otpSuccessAction(true));
         await StorageService.removeKey(StorageConstants.USER_OTP);
     } else {
         dispatch(otpFailureAction('OTP invalid'));
     }
-
 };
 
 export const otpInit = () => async (dispatch: Dispatch) => {
     dispatch(otpInitAction());
 };
-
