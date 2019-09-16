a4ba39e0f7f9392181798a5f464ebda64e328f11
Appu Patil <apatil@goavega.com>
2019-09-16 15:07:13 +0530

    Added dimensions dependency

--- src/screens/leads/filtered-leads.tsx
+++ src/screens/leads/filtered-leads.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, ListView, Platform, ActivityIndicator } from 'react-native';
+import { FlatList, ListView, Platform, ActivityIndicator, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
 import {
     View,
