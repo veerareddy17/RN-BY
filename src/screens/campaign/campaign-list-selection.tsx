5c47ec759e15ce4add0bb9ab5dad6952a577f2a3
Appu Patil <apatil@goavega.com>
2019-09-27 15:54:38 +0530

    allowing android src

--- src/screens/campaign/campaign-list-selection.tsx
+++ src/screens/campaign/campaign-list-selection.tsx
@@ -21,7 +21,7 @@ import { AppState } from '../../redux/store';
 import { NetworkContext } from '../../provider/network-provider';
 import { NavigationScreenProp } from 'react-navigation';
 import { selectedCampaign } from '../../redux/actions/campaign-actions';
-import { StatusBar, Alert, Platform } from 'react-native';
+import { StatusBar, Alert, Platform, ActivityIndicator } from 'react-native';
 import { AlertError } from '../error/alert-error';
 import { ToastError } from '../error/toast-error';
 import { logout } from '../../redux/actions/user-actions';
@@ -36,7 +36,7 @@ export interface CampaignListProps {
     logout(): (dispatch: Dispatch<AnyAction>) => Promise<void>;
 }
 
-export interface CampaignListState { }
+export interface CampaignListState {}
 
 class CampaignList extends Component<CampaignListProps, CampaignListState> {
     static contextType = NetworkContext;
@@ -91,45 +91,42 @@ class CampaignList extends Component<CampaignListProps, CampaignListState> {
                         </Right>
                     </Header>
                 ) : (
-                        <Header style={{ backgroundColor: '#813588' }} androidStatusBarColor="#813588">
-                            <Body>
-                                <Title style={{ color: 'white', fontSize: 18, marginLeft: 10, fontFamily: 'system font' }}>
-                                    Select Campaign
+                    <Header style={{ backgroundColor: '#813588' }} androidStatusBarColor="#813588">
+                        <Body>
+                            <Title style={{ color: 'white', fontSize: 18, marginLeft: 10, fontFamily: 'system font' }}>
+                                Select Campaign
                             </Title>
-                            </Body>
-                            <Right>
-                                <Button transparent onPress={this.confirmLogout}>
-                                    <Icon name="ios-log-out" style={{ color: 'white' }} />
-                                </Button>
-                            </Right>
-                        </Header>
-                    )}
+                        </Body>
+                        <Right>
+                            <Button transparent onPress={this.confirmLogout}>
+                                <Icon name="ios-log-out" style={{ color: 'white' }} />
+                            </Button>
+                        </Right>
+                    </Header>
+                )}
                 <Content>
                     {this.props.campaignState.isLoading ? (
-                        <View>
-                            <Spinner />
-                            <Text style={{ textAlign: 'center' }}>Fetching Campaigns...</Text>
-                        </View>
+                        <ActivityIndicator style={{ marginTop: 20 }} animating size="large" />
                     ) : (
-                            <View>
-                                <List>
-                                    {this.props.campaignState.campaignList.map(campaign => {
-                                        return (
-                                            <ListItem
-                                                button={true}
-                                                key={campaign.id}
-                                                onPress={() => this.handleSelections(campaign)}
+                        <View>
+                            <List>
+                                {this.props.campaignState.campaignList.map(campaign => {
+                                    return (
+                                        <ListItem
+                                            button={true}
+                                            key={campaign.id}
+                                            onPress={() => this.handleSelections(campaign)}
                                             style={{ marginRight: 17 }}
-                                            >
-                                                <Left>
-                                                    <Text style={{ fontFamily: 'system font' }}>{campaign.name}</Text>
-                                                </Left>
-                                            </ListItem>
-                                        );
-                                    })}
-                                </List>
-                            </View>
-                        )}
+                                        >
+                                            <Left>
+                                                <Text style={{ fontFamily: 'system font' }}>{campaign.name}</Text>
+                                            </Left>
+                                        </ListItem>
+                                    );
+                                })}
+                            </List>
+                        </View>
+                    )}
                 </Content>
                 {!this.context.isConnected && (
                     <View
